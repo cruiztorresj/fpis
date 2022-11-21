ab660b971e2077ed509951b8461ce5874f25cb82
Caleb Ruiz <calebjosue-dev@outlook.com>
2022-11-20 22:24:39 -0600

    master: Exercises 5.2 and 5.3

--- src/main/scala/chapter5/MyLazyList.scala
+++ src/main/scala/chapter5/MyLazyList.scala
@@ -26,6 +26,55 @@ enum MyLazyList[+A]:
 			
 		loop(List.empty[A], this)
 
+	// Exercise 5.2
+	// def take(n: Int): MyLazyList[A] =
+	// 	@annotation.tailrec
+	// 	def loop(acc: MyLazyList[A], lazyList: => MyLazyList[A], n: Int): MyLazyList[A] =
+	// 		if n == 0 then
+	// 			acc
+	// 		else
+	// 			lazyList match
+	// 				case Empty => acc
+	// 				case Cons(hd, tl) => loop (MyLazyList.cons(hd(), acc), tl(), n - 1)
+		
+	// 	if n < 0 then Empty
+	// 	else loop(Empty, this, n)
+
+	// Exercise 5.2
+	// a) Define take(n) for returning the first n elements of a LazyList.
+	// I've ended having to take a look at the provided answer.
+	// It turns out 
+	def take(n: Int): MyLazyList[A] =
+		this match
+			case Cons(h, t) if n > 1 => MyLazyList.cons(h(), t().take(n - 1))
+			case Cons(h, t) if n == 1 => MyLazyList.cons(h(), MyLazyList.empty)
+			case _ => MyLazyList.empty
+
+	// Exercise 5.2
+	// b) Define drop(n) for skipping thr first n elements of a LazyList.
+	def drop(n: Int): MyLazyList[A] =
+		if n < 0 || n == 0 then
+			this
+		else
+			@annotation.tailrec
+			def loop(counter: Int, result: MyLazyList[A]): MyLazyList[A] =
+				if counter == 0 then
+					result
+				else
+					result match
+						case Cons(head, tail) => loop(counter - 1, tail())
+						case Empty => Empty
+
+			loop(n, this)
+
+	// Exercise 5.3
+	// Write a function takeWhile for returning all starting elements of a LazyList
+	// that match the given predicate.
+	def takeWhile(p: A => Boolean): MyLazyList[A] =
+		this match
+			case Cons(hd, tl) if p(hd()) => MyLazyList.cons(hd(), tl().takeWhile(p))
+			case Cons(hd, tl) if !p(hd()) => MyLazyList.empty
+			case _ => MyLazyList.empty
 
 object MyLazyList:
 	def cons[A](hd: => A, tl: => MyLazyList[A]): MyLazyList[A] =
